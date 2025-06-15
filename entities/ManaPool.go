ea022b26acbcaa3111cce4dc298f6e1185c1d88a
Matt Enterline <matthew.j.enterline@gmail.com>
2025-06-15 15:06:21 -0400

    added mana pool and tests for playing a land and casting a spell

--- entities/ManaPool.go
+++ entities/ManaPool.go
@@ -1,5 +1,8 @@
 package entities
 
+import "errors"
+
+// Not really "mana" pool, but resources to create mana
 type ManaPool struct {
 	AvailableMana map[Mana]int
 }
@@ -13,4 +16,21 @@ func (m *ManaPool) AddtoPool(mana Mana, amount int) {
 		m.AvailableMana = make(map[Mana]int)
 	}
 	m.AvailableMana[mana] += amount
+}
+
+func (m *ManaPool) RemoveFromPool(mana Mana) error {
+	if m.AvailableMana == nil {
+		return errors.New("mana pool is empty")
+	}
+	m.AvailableMana[mana] = m.AvailableMana[mana] - 1
+	if m.AvailableMana[mana] <= 0 {
+		delete(m.AvailableMana, mana)
+	}
+	return nil
+}
+
+func (m *ManaPool) CastSpell(spell Spell) {
+	for _, mana := range spell.GetManaCost() {
+		m.RemoveFromPool(mana)
+	}
 }
--- entities/test/ManaPool_test.go
+++ entities/test/ManaPool_test.go
@@ -27,5 +27,45 @@ func TestAddToPool(t *testing.T) {
 	got := manaPool.GetMana()
 	if got[entities.Red] != 2 || got[entities.Blue] != 1 {
 		t.Errorf("Expected mana pool to have 2 Red and 1 Blue mana, got %v", got)
-	}	
+	}
+}
+
+func TestRemoveFromPoolValid(t *testing.T) {
+	manaMap := map[entities.Mana]int{
+		entities.Red:   2,
+		entities.Blue:  1,
+		entities.Green: 3,
+	}
+	manaPool := entities.ManaPool{AvailableMana: manaMap}
+	err1 := manaPool.RemoveFromPool(entities.Blue)
+	if err1 != nil {
+		t.Errorf("Expected no error, got %v", err1)
+	}
+	err2 := manaPool.RemoveFromPool(entities.Green)
+	if err2 != nil {
+		t.Errorf("Expected no error, got %v", err2)
+	}
+	got := manaPool.GetMana()
+	if got[entities.Red] != 2 || got[entities.Blue] != 0 || got[entities.Green] != 2 {
+		t.Errorf("Expected mana pool to have 2 Red, 0 Blue, and 3 Green mana, got %v", got)
+	}
+}
+
+func TestCastSpellValid(t *testing.T) {
+
+	manaMap := map[entities.Mana]int{
+		entities.Red:   2,
+		entities.Blue:  3,
+		entities.Green: 1,
+	}
+	testManaPool := entities.ManaPool{AvailableMana: manaMap}
+	testSpell := entities.Spell{
+		Cost:     []entities.Mana{entities.Blue, entities.Blue, entities.Green},
+		CardType: entities.Creature}
+	testManaPool.CastSpell(testSpell)
+	got := testManaPool.GetMana()
+	if got[entities.Red] != 2 || got[entities.Blue] != 1 || got[entities.Green] != 0 {
+		t.Errorf("Expected mana pool to have 2 Red, 1 Blue, and 0 Green mana, got %v", got)
+	}
+
 }
