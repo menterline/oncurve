42b1169c50bd4b418c12c6b0d10dce5e923948f6
Matt Enterline <menterline.dev@gmail.com>
2025-07-19 22:09:56 -0400

    added CMCs back

--- templates/SimSettings/SimSettings.go
+++ templates/SimSettings/SimSettings.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+type ConvertedManaCosts struct {
+	CMCs []int
+}
+
 func ServeSimSettings(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/SimSettings/index.html"))
-	if err := tmpl.Execute(w, nil); err != nil {
+	data := ConvertedManaCosts{
+		CMCs: []int{1, 2, 3, 4, 5, 6, 7, 8},
+	}
+	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
