d3852bbba0020cd17358ee01966ec66c3e2ff6e0
Matt Enterline <menterline.dev@gmail.com>
2025-08-29 21:49:13 -0400

    went down to 4drops, and made formatting way nicer

--- templates/SimSettings/SimSettings.go
+++ templates/SimSettings/SimSettings.go
@@ -12,7 +12,7 @@ type ConvertedManaCosts struct {
 func ServeSimSettings(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/SimSettings/index.html"))
 	data := ConvertedManaCosts{
-		CMCs: []int{1, 2, 3, 4, 5, 6, 7, 8},
+		CMCs: []int{1, 2, 3, 4 },
 	}
 	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
